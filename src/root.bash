e18753b439dec11513dbe67e40aa01ccbe0053ee
MaxMEllon <maxmellon1994@gmail.com>
2018-04-16 21:03:50 +0900

    Implemented `git_branch`

--- src/root.bash
+++ src/root.bash
@@ -56,3 +56,14 @@ has_env() {
 is_git_repo() {
   git rev-parse --is-inside-work-tree &> /dev/null 2>&1  && return $true || return $false
 }
+
+git_branch() {
+  if is_git_repo; then
+    branch=$(git symbolic-ref --short HEAD)
+    echo $branch
+    return 0
+  else
+    echo "X"
+    return 1
+  fi
+}
--- test/test.bash
+++ test/test.bash
@@ -88,3 +88,17 @@ describe "is_git_repo" && {
 
   assert_end is_git_repo
 }
+
+describe "git_branch" && {
+  it "gitリポジトリで実行したときbranch名が得られることを期待" && {
+    assert_raises "git branch | grep `git_branch`" "0"
+  }
+
+  it "gitリポジトリ外で実行したときXが得られることを期待" && {
+    cd ~  &> /dev/null 2>&1
+    assert "git_branch" "X"
+    cd - &> /dev/null 2>&1
+  }
+
+  assert_end git_branch
+}
