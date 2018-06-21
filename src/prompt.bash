a5e5fa6a61080eb4c3e6551331de49f4107e0cf2
MaxMEllon <maxmellon1994@gmail.com>
2018-06-21 22:24:00 +0900

    :art: Refactoring

--- src/prompt.bash
+++ src/prompt.bash
@@ -1,25 +1,24 @@
 true=0
 false=1
 
-border-line-by-number() {
-  : ${1:?}
-  local lines=`tput cols`
-  for ((i=0; i<$1; i++)); do
-    printf "-"
-  done
-}
-
-border-line() {
-  local lines=`tput cols`
-  border-line-by-number $lines
-}
-
 # Repeat character by number
 # $1 string - repeated character
 # $2 number - repeat count
 repeat-char-by-number() {
   : ${2:?}
+  local str=""
   for ((i=0; i<$2; i++)); do
-    printf "$1"
+    str="$str$1"
   done
+  printf "$str"
+}
+
+border-line-by-number() {
+  : ${1:?}
+  repeat-char-by-number "-" $1
+}
+
+border-line() {
+  local lines=`tput cols`
+  border-line-by-number $lines
 }
